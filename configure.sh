a6fd68131dba3242bcc30b21ed5f21df09d24649
Connor Boyle <cjblink1@gmail.com>
2018-09-16 15:39:53 -0400

    auth-keys properties

---
+++ configure.sh
@@ -0,0 +1,7 @@
+kubectl apply -f admin-user-cluster-role-binding.yaml
+kubectl apply -f admin-user-service-account.yaml
+helm init --service-account=admin-user
+helm repo update
+helm install --name cert-manager --namespace kube-system stable/cert-manager
+kubectl create secret generic srtsignin-role-secret --from-file=./auth-keys.properties
+kubectl apply -f .
