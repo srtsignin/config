6ae9e320df1a0be7afa531689601336e9087cd03
cKeinsley <keinsleyChris@gmail.com>
2018-10-16 13:42:41 -0400

    updated configs for cardfire

--- configure.sh
+++ configure.sh
@@ -4,4 +4,5 @@ helm init --service-account=admin-user
 helm repo update
 helm install --name cert-manager --namespace kube-system stable/cert-manager
 kubectl create secret generic srtsignin-role-secret --from-file=./auth-keys.properties
+kubectl create secret generic srtsignin-cardfire-secret --from-file=./secrets-cf.properties
 kubectl apply -f .
