01cf52ba6026841dfd485050652ecb5a9dcfb26d
Collin Moore <collin.moore96@gmail.com>
2018-10-31 15:22:09 -0400

    Fixed the configure script to properly add the data-service-conf secret. Also updated the active-users-config

--- configure.sh
+++ configure.sh
@@ -11,5 +11,5 @@ kubectl create secret generic srtsignin-data-service-secret --from-file=./secret
 kubectl create secret generic srtsignin-role-secret -n stage --from-file=./stage-secrets/auth-keys.properties
 kubectl create secret generic srtsignin-cardfire-secret -n stage --from-file=./stage-secrets/secrets-cf.properties
 kubectl create secret generic srtsignin-active-users-secret -n stage --from-file=./stage-secrets/active-users-config.json
-kubectl create secret generic srtsignin-data-service-secret -n stage --from-file=./stage/secrets/data-service-conf.json
+kubectl create secret generic srtsignin-data-service-secret -n stage --from-file=./stage-secrets/data-service-conf.json
 kubectl apply -f .
